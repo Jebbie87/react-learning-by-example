3c941ebeb80832fd787eaac5cc1bfc589467d1be
Jebbie87 <jeffreycj.chang@gmail.com>
2017-03-01 17:35:29 -0500

    First few components made

--- catch-of-the-day/src/components/StorePicker.jsx
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 
 export default class StorePicker extends Component {
-  render{
+  render() {
     return (
-
+      <form className='store-selector'>
+        {/* test comment */}
+        <h2>Please enter a store</h2>
+        <input type='text' required placeholder='Store Name' />
+        <button type='submit'>Visit a store -> </button>
+      </form>
     )
   }
 }
