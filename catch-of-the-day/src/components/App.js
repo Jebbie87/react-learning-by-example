051f0b7d319a373b0cff2978a2984af3954dd357
Jebbie87 <jeffreycj.chang@gmail.com>
2017-03-02 12:54:34 -0500

    Finished lesson 20

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -16,6 +16,7 @@ export default class App extends Component {
     this.addFish = this.addFish.bind(this)
     this.loadSamples = this.loadSamples.bind(this)
     this.addToOrder = this.addToOrder.bind(this)
+    this.updateFish = this.updateFish.bind(this)
   }
 
   componentWillMount() {
@@ -32,6 +33,12 @@ export default class App extends Component {
 
   componentWillUpdate(nextProps, nextState) {
     localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order))
+  }
+
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes}
+    fishes[key] = updatedFish
+    this.setState({ fishes: fishes })
   }
 
   addFish(fish) {
@@ -72,7 +79,7 @@ export default class App extends Component {
           </ul>
         </div>
           <Order fishes={this.state.fishes} order={this.state.order} params={this.props.params}/>
-          <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+          <Inventory updateFish={this.updateFish} fishes={this.state.fishes} addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
